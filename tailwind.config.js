2dce835a8cfec4023d1b491f43ded951d750892c
NandeshMM <nandeshsahyadri@gmail.com>
2024-09-10 15:50:03 +0530

    Toggle Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
   content: [
     './src/**/*.{js,jsx,ts,tsx}',  // All JS, JSX, TS, and TSX files in the src folder
     './public/index.html',
