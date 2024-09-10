703524e43190a07e2f5c6acc500f8938dec0699b
NandeshMM <nandeshsahyadri@gmail.com>
2024-09-10 11:03:22 +0530

    Second Changes Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',  // All JS, JSX, TS, and TSX files in the src folder
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
